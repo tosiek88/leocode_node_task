cb45bddf1eac64dad92a415946005d2e4126d3c4
tocha <Mateusz Tocha>
2021-07-04 11:27:28 +0200

    Implement Roles, and AuthGuards

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UsersService } from 'src/users/users.service';
-import { JwtPayload } from './jwt.interface';
+import { JwtPayload } from '../types/jwt.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
