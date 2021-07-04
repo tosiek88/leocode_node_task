a40d9a3a52a6c1b7f8bef014d04633c6e5e74f8a
tocha <Mateusz Tocha>
2021-07-04 21:56:57 +0200

    Add basic e2e-test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,11 +5,14 @@ import { UsersModule } from '../src/users/users.module';
 import { EncryptModule } from '../src/encrypt/encrypt.module';
 import { HttpModule, INestApplication } from '@nestjs/common';
 import { AppController } from '../src/app.controller';
+import { IKeyPair } from 'src/users/users.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let token: string;
+  let privateKey, publicKey: string;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AuthModule, UsersModule, EncryptModule, HttpModule],
       controllers: [AppController],
@@ -18,11 +21,73 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
+  describe('/api/sign-in (POST)', () => {
+    it(' It should return 401 Unauthorized due lack of credentials in body ', () => {
+      return request(app.getHttpServer()).post('/api/sign-in').expect(401);
+    });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    it(' It should 200 and return generatedKeys', async () => {
+      const result = await request(app.getHttpServer())
+        .post('/api/sign-in')
+        .send({
+          email: 'admin@mail.com',
+          password:
+            '$2b$10$NVZuUn6qxpPtmHkLDHxQzetkKgT4Oopi87KOQJcZFFrOm0LdYtWae',
+        });
+
+      expect(result.statusCode).toBe(201);
+      expect(result.body).toHaveProperty('authToken');
+      token = result.body.authToken;
+    });
+  });
+
+  describe('/api/generate-key-pair (POST)', () => {
+    it('Should return 401 Unauthorized due lack of authentication token ', async () => {
+      const result = await request(app.getHttpServer())
+        .post('/api/generate-key-pair')
+        .expect(401);
+    });
+
+    it('Should return pair key', async () => {
+      const result = await request(app.getHttpServer())
+        .post('/api/generate-key-pair')
+        .set('Authorization', `Bearer ${token}`)
+        .send();
+
+      expect(result.statusCode).toBe(201);
+      privateKey = (result.body as IKeyPair).privateKey;
+      publicKey = (result.body as IKeyPair).publicKey;
+      expect(privateKey).not.toEqual('');
+      expect(publicKey).not.toEqual('');
+    });
+  });
+
+  describe('/api/encrypt (POST)', () => {
+    it('Should return 401 Unauthorized due lack of authentication token ', async () => {
+      const result = await request(app.getHttpServer())
+        .post('/api/encrypt')
+        .send();
+
+      expect(result.statusCode).toBe(401);
+    });
+
+    it('Should return base64 encrypted string', async () => {
+      const encryptResult = await request(app.getHttpServer())
+        .post('/api/encrypt')
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', `application/json`)
+        .send({});
+      expect(encryptResult.statusCode).toBe(201);
+      expect(encryptResult.body).toHaveProperty('encrypted');
+      expect(encryptResult.body.encrypted).not.toBe('');
+
+      const decryptResult = await request(app.getHttpServer())
+        .post('/api/decrypt')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ encrypted: encryptResult.body.encrypted });
+
+      expect(decryptResult.statusCode).toBe(201);
+      console.debug(decryptResult.body);
+    }, 10000);
   });
 });
