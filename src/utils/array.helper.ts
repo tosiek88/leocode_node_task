e7620ee7e9a519a1d09a6f11af1b6d36ed96a1d3
tocha <Mateusz Tocha>
2021-07-04 20:39:23 +0200

    Create helper function for Buffer

---
+++ src/utils/array.helper.ts
@@ -0,0 +1,12 @@
+export function chunkArray(myArray: Buffer, chunk_size: number) {
+  let index = 0;
+  const arrayLength = myArray.length;
+  const tempArray = [];
+
+  for (index = 0; index < arrayLength; index += chunk_size) {
+    const myChunk = myArray.slice(index, index + chunk_size);
+    // Do something if you want with the group
+    tempArray.push(myChunk);
+  }
+  return tempArray;
+}
